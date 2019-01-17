d444bf619a470e8aa5c2b5f9bc4a4ec319189cb8
rudineybarbosa <rudiney1h@gmail.com>
2019-01-17 19:13:26 -0300

    Adicionando id, alt e src à tag <img>

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const ImageList = (props) => {
     //console.log(props.images);
-    const images = props.images.map(image => {
+    const images = props.images.map(({id, description, urls}) => {
         
-        return <img src={image.urls.regular} />
-        
+        return (
+            <div id={id}>
+                <img alt={description} src={urls.regular} />
+            </div>
+            )
     });
 
 
